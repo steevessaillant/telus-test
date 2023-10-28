9db0c57fa0597dff0b0519cf3fb8b816c6467e99
Steeves Saillant <steevessaillant@hotmail.com>
2023-10-28 08:46:19 -0700

    Generate the glue code for the gherkin file

---
+++ src/test/java/com/telus/tv/StepDefinitions.java
@@ -0,0 +1,44 @@
+package com.telus.tv;
+
+import io.cucumber.java.en.*;
+
+import org.junit.jupiter.api.Assertions.*;
+
+public class StepDefinitions {
+
+    @Given("I am on the TELUS TV+ website")
+    public void iAmOnTheTELUSTVWebsite() {
+    }
+
+    @When("I close the welcome popup")
+    public void iCloseTheWelcomePopup() {
+    }
+
+    @And("I click on {string}")
+    public void iClickOn(String arg0) {
+    }
+
+    @And("I scroll to the {string} section")
+    public void iScrollToTheSection(String arg0) {
+    }
+
+    @And("I click on the {string} button")
+    public void iClickOnTheButton(String arg0) {
+    }
+
+    @And("I select {string} from the filter options")
+    public void iSelectFromTheFilterOptions(String arg0) {
+    }
+
+    @And("I click the {string} button")
+    public void iClickTheButton(String arg0) {
+    }
+
+    @Then("I should find an asset with a rating of {int}+")
+    public void iShouldFindAnAssetWithARatingOf(int arg0) {
+    }
+
+    @And("I click on the asset to open the details page")
+    public void iClickOnTheAssetToOpenTheDetailsPage() {
+    }
+}
